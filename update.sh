e2a992e2519ccb9c571f97cedde76bba6b928259
Frank Liang <frankliang@Franks-MacBook-Pro.local>
2023-11-19 00:28:08 +0000

    update version number

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-echo "Enter Your Message\n"
+echo "What change have you made since last commit?:\n"
 read message
 
 R -e "roxygen2::roxygenize()"
-R -e "pkgdown::build_site_github_pages()"
+#R -e "pkgdown::build_site_github_pages()"
 git add .
 git commit -m"${message}"
 git status
